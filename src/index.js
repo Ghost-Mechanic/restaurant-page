b2e2b96de2c17d85396f4cfa984ccd52e406fee8
Ghost-Mechanic <158665405+Ghost-Mechanic@users.noreply.github.com>
2024-06-26 03:20:37 -0500

    Added styling

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ const aboutButton = document.querySelector("#about");
 
 // call createHomepage to initialize webpage with homepage content
 createHomepage();
+homeButton.classList.add("active-button");
 
 const content = document.querySelector("#content");
 
@@ -16,15 +17,24 @@ const content = document.querySelector("#content");
 // with their appropriate content by calling module function
 homeButton.addEventListener("click", () => {
     content.innerHTML = "";
+    menuButton.classList.remove("active-button");
+    aboutButton.classList.remove("active-button");
+    homeButton.classList.add("active-button");
     createHomepage();
 });
 
 menuButton.addEventListener("click", () => {
     content.innerHTML = "";
+    homeButton.classList.remove("active-button");
+    aboutButton.classList.remove("active-button");
+    menuButton.classList.add("active-button");
     createMenu();
 });
 
 aboutButton.addEventListener("click", () => {
     content.innerHTML = "";
+    menuButton.classList.remove("active-button");
+    homeButton.classList.remove("active-button");
+    aboutButton.classList.add("active-button");
     createAboutPage();
 });
